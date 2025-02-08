6d762024f9c749b2fea22e045e374d36b13f68f9
Muliminty <313195303@qq.com>
2025-02-08 12:46:53 +0800

    feat: 改为静态编译

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export", // Next.js 13 之后支持的写法
+  trailingSlash: true, // 让导出的 HTML 以 `.html` 结尾
+}
 
 module.exports = nextConfig
